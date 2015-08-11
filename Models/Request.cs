4f861a2a0a0b0abecd59dfc95d7f8624c17fdeba
Mikael Mengistu <mikaelm@mit.edu>
2015-08-10 18:39:15 -0700

    Initial commit

---
+++ Models/Request.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataProxy.Models
+{
+    public class Request
+    {
+        [Key]
+        public int Id { get; set; }
+        public string MethodType { get; set; }
+        public string Url { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
